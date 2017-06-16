5245b4ee814df418f074a83f8aaa0c57e54fee49
has85 <hasanov85@gmail.com>
2017-06-16 16:37:29 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by nurlan.hasanov on 16.06.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int a = 3 + 2 * (5+1);
+        System.out.println(a);
+    }
+}
